7b331074845230c67821b327fdca7979d4635a5d
VictorTao1998 <jit124@ucsd.edu>
2021-04-29 08:04:33 -0700

    1

--- CasStereoNet/scripts/kitti12.sh
+++ CasStereoNet/scripts/kitti12.sh
@@ -2,17 +2,16 @@
 #set -x
 export PYTHONWARNINGS="ignore"
 
-save_path="./checkpoints"
-ngpu=1
+save_path=$2
 
 if [ ! -d $save_path ];then
     mkdir -p $save_path
 fi
 
-DATAPATH="./data/data_scene_flow_2012/"
+DATAPATH="/cephfs/datasets/iccv_pnp/messy-table-dataset/v5/training"
 
-python -m torch.distributed.launch --nproc_per_node=$ngpu main.py --dataset kitti \
-    --datapath $DATAPATH --trainlist ./filenames/kitti12_train.txt --testlist ./filenames/kitti12_val.txt \
+python -m torch.distributed.launch --nproc_per_node=$1 main.py --dataset kitti \
+    --datapath $DATAPATH --trainlist /cephfs/datasets/iccv_pnp/messy-table-dataset/v5/training_lists/200_train.txt --testlist /cephfs/datasets/iccv_pnp/messy-table-dataset/v5/training_lists/200_val.txt \
     --test_datapath $DATAPATH --test_dataset kitti \
     --epochs 300 --lrepochs "200:10" \
     --crop_width 512  --crop_height 256 --test_crop_width 1248  --test_crop_height 384 \
