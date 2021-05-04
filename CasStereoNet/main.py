9e9feecc9a88fb4154cd9cdff42888930c832fa2
VictorTao1998 <jit124@ucsd.edu>
2021-05-04 05:54:38 -0700

    a

--- CasStereoNet/main.py
+++ CasStereoNet/main.py
@@ -216,6 +216,8 @@ def train():
 
         # training
         for batch_idx, sample in enumerate(TrainImgLoader):
+            if batch_idx == 100:
+                break
             global_step = len(TrainImgLoader) * epoch_idx + batch_idx
             start_time = time.time()
             do_summary = global_step % args.summary_freq == 0
