ee22eb096b6f055b6cfd4428a0bc21ef59f01157
VictorTao1998 <jit124@ucsd.edu>
2021-05-05 22:05:29 -0700

    a

--- CasStereoNet/main.py
+++ CasStereoNet/main.py
@@ -360,6 +360,7 @@ def test_sample(sample, compute_metrics=True):
     depest = disp_ests[0].cpu().numpy()[0]
     depest = depest[228:,:960]
     maskest = (depest < args.maxdisp) & (depest > 0)
+    print(type(depest), depest.shape)
     depest = np.nan_to_num(f*b/depest)
 
     dep_err_map = depest - dep_gt
