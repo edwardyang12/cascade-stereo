3389d6e93c0db429b3a074ad13398c9dffb8b196
VictorTao1998 <jit124@ucsd.edu>
2021-06-04 21:31:16 -0700

    a

--- CasStereoNet/scripts/kitti12.sh
+++ CasStereoNet/scripts/kitti12.sh
@@ -2,7 +2,7 @@
 #set -x
 export PYTHONWARNINGS="ignore"
 
-save_path="/cephfs/jianyu/eval/mt_real_output"
+save_path="/cephfs/jianyu/eval/mt_sim_output"
 
 if [ ! -d $save_path ];then
     mkdir -p $save_path
@@ -12,11 +12,11 @@ DATAPATH="/cephfs/datasets/iccv_pnp/real_data_v9/"
 pip install --upgrade pip setuptools wheel
 pip install -r /cephfs/jianyu/mt_real_eval/requirements.txt
 python -m torch.distributed.launch --nproc_per_node=1 /cephfs/jianyu/mt_real_eval/CasStereoNet/main.py --dataset kitti \
-    --datapath /cephfs/datasets/iccv_pnp/real_data_v9/ --trainlist /cephfs/datasets/iccv_pnp/real_data_v9/test_list.txt --testlist /cephfs/datasets/iccv_pnp/real_data_v9/test_list.txt \
-    --test_datapath /cephfs/datasets/iccv_pnp/real_data_v9/ --test_dataset kitti \
+    --datapath /cephfs/datasets/iccv_pnp/messy-table-dataset/real_v9/training --trainlist /cephfs/datasets/iccv_pnp/messy-table-dataset/real_v9/training_lists/all.txt --testlist /cephfs/datasets/iccv_pnp/messy-table-dataset/real_v9/training_lists/all.txt \
+    --test_datapath /cephfs/datasets/iccv_pnp/messy-table-dataset/real_v9/training --test_dataset kitti \
     --epochs 300 --lrepochs "200:10" \
     --crop_width 512  --crop_height 256 --test_crop_width 1248  --test_crop_height 768 \
     --ndisp "48,24" --disp_inter_r "4,1" --dlossw "0.5,2.0"  --using_ns --ns_size 3 \
-    --model gwcnet-c --logdir "/cephfs/jianyu/eval/mt_real_output"  --ndisps "48,24" \
+    --model gwcnet-c --logdir "/cephfs/jianyu/eval/mt_sim_output"  --ndisps "48,24" \
     --disp_inter_r "4,1"   --batch_size 2 --mode test \
     --loadckpt "/cephfs/jianyu/mtcaseval_train_output/checkpoint_best.ckpt"
