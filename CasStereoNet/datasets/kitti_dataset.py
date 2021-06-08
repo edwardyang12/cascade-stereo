dc4be0c90dd1cfb467afb487a5fca985377041a7
VictorTao1998 <jit124@ucsd.edu>
2021-06-08 09:49:02 -0700

    a

--- CasStereoNet/datasets/kitti_dataset.py
+++ CasStereoNet/datasets/kitti_dataset.py
@@ -46,8 +46,6 @@ class KITTIDataset(Dataset):
 
     def load_image(self, filename, half):
         img = Image.open(filename).convert('RGB')
-        if not rgb:
-            img = Image.open(filename).convert('I;16')
         if half:
             img = img.resize((int(img.size[0]/2),int(img.size[1]/2)))
         return img
