157bf72a9178befadb361676f115a0d0ab432a24
VictorTao1998 <jit124@ucsd.edu>
2021-06-10 10:51:22 -0700

    a

--- CasStereoNet/main.py
+++ CasStereoNet/main.py
@@ -380,6 +380,7 @@ def test_sample(sample, compute_metrics=True):
     disp_gt_rgb = disp_rgb.reshape((1,1,540,960)).cuda()
     label = label.reshape((1,1,540,960)).cuda()
     disparity_L_from_R = apply_disparity_cu(disp_gt_t, disp_gt_t.int())
+    print(torch.max(label), torch.max(disp_gt_rgb.int()))
     label_rgb = apply_disparity_cu(label, disp_gt_rgb.int())
 
 
