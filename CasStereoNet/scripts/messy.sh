57f5f31e0585aaf80904c04fb51846ffc568d968
VictorTao1998 <jit124@ucsd.edu>
2021-06-23 00:41:49 -0700

    a

--- CasStereoNet/scripts/messy.sh
+++ CasStereoNet/scripts/messy.sh
@@ -9,15 +9,36 @@ if [ ! -d $save_path ];then
 fi
 
 DATAPATH="/cephfs/datasets/iccv_pnp/real_data_v9/"
-pip install --upgrade pip setuptools wheel
-pip install -r /cephfs/jianyu/cs_eval/requirements.txt
-python -m torch.distributed.launch --nproc_per_node=1 /cephfs/jianyu/cs_eval/CasStereoNet/main.py --dataset messy_table \
-    --datapath /cephfs/datasets/iccv_pnp/real_data_v9/ --trainlist /cephfs/datasets/iccv_pnp/real_data_v9/test_list.txt --testlist /cephfs/datasets/iccv_pnp/real_data_v9/test_list.txt \
-    --test_datapath /cephfs/datasets/iccv_pnp/real_data_v9/ --test_dataset messy_table \
+
+python -m torch.distributed.launch --nproc_per_node=1 /cephfs/jianyu/cs_eval/CasStereoNet/main.py
+    --dataset messy_table \
+    --test_dataset messy_table \
+    --datapath /cephfs/datasets/iccv_pnp/messy-table-dataset/v9/training \
+    --trainlist /cephfs/datasets/iccv_pnp/messy-table-dataset/v9/training_lists/all_train.txt \
+    --test_datapath /cephfs/datasets/iccv_pnp/messy-table-dataset/v9/training \
+    --testlist /cephfs/datasets/iccv_pnp/messy-table-dataset/v9/training_lists/all_val.txt \
+    --test_sim_datapath /cephfs/datasets/iccv_pnp/messy-table-dataset/real_v9/training \
+    --sim_testlist /cephfs/datasets/iccv_pnp/messy-table-dataset/real_v9/training_lists/all.txt \
+    --test_real_datapath /cephfs/datasets/iccv_pnp/real_data_v9/ \
+    --real_testlist /cephfs/datasets/iccv_pnp/real_data_v9/test_list.txt \
     --depthpath /cephfs/datasets/iccv_pnp/messy-table-dataset/real_v9/training \
-    --epochs 300 --lrepochs "200:10" \
-    --crop_width 512  --crop_height 256 --test_crop_width 1248  --test_crop_height 768 \
-    --ndisp "48,24" --disp_inter_r "4,1" --dlossw "0.5,2.0"  --using_ns --ns_size 3 \
-    --model gwcnet-c --logdir "/cephfs/jianyu/eval/cs_real_eval"  --ndisps "48,24" \
-    --disp_inter_r "4,1"   --batch_size 2 --real --mode test --summary_freq 500 ----test_summary_freq 50\
-    --loadckpt "/cephfs/jianyu/train/cs_train/checkpoint_best.ckpt"
+    --epochs 300 \
+    --lrepochs "200:10" \
+    --crop_width 512  \
+    --crop_height 256 \
+    --test_crop_width 1248  \
+    --test_crop_height 768 \
+    --ndisp "48,24" \
+    --disp_inter_r "4,1" \
+    --dlossw "0.5,2.0"  \
+    --using_ns \
+    --ns_size 3 \
+    --model gwcnet-c \
+    --logdir "/cephfs/jianyu/eval/cs_eval"  \
+    --ndisps "48,24" \
+    --disp_inter_r "4,1"  \
+    --batch_size 2 \
+    --mode train \
+    --summary_freq 500 \
+    --test_summary_freq 50 \
+    #--loadckpt "/cephfs/jianyu/train/cs_train/checkpoint_best.ckpt"
